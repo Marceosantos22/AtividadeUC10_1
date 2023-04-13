0b9a133d0e9be6ada3a62d3724a1b7db02c947bd
Marcelo Oliveira <marceosantos@gmail.com>
2023-04-12 21:51:53 -0300

    alterado no da tela principal + classe + Add em Libraries MySql-connector-j8.0.32.jar

---
+++ src/Data/CadastroFilmesDAO.java
@@ -0,0 +1,63 @@
+package Data;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class CadastroFilmesDAO {
+
+	Connection conn;
+	PreparedStatement st;
+	ResultSet rs;
+
+	public boolean conectar() {
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cenaflix", "root", "F17101818@");
+
+			return true;
+
+		} catch (ClassNotFoundException | SQLException ex) {
+
+			System.out.println("Erro ao conectar: " + ex.getMessage());
+			return false;
+
+		}
+
+	}
+
+	public void desconectar() {
+		try {
+
+			conn.close();
+
+		} catch (SQLException e) {
+
+		}
+
+	}
+
+	public int salvar(CadastroFilmes cadastroFilmes) {
+
+		int status;
+
+		try {
+			st = conn.prepareStatement("INSERT INTO filmes  (nome, datalancamento, categoria) VALUES(?,?,?)");
+			st.setString(1, cadastroFilmes.getNomeDoFilme());
+			st.setDate(2, new java.sql.Date(cadastroFilmes.getDataLacamento().getTime()));
+			st.setString(3, cadastroFilmes.getCategoria());
+
+			status = st.executeUpdate();
+			return status;
+
+		} catch (SQLException ex) {
+
+			System.out.println("Erro ao conectar: " + ex.getMessage());
+			return ex.getErrorCode();
+
+		}
+	}
+}
